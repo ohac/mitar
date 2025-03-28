97202b38fbce4c81a3f9b920cd22692ccca92b69
OHASHI Hideya <ohachige@gmail.com>
2025-03-28 19:30:33 +0900

    fix style

--- fetchall.sh
+++ fetchall.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 for npub in $(cat npubs.txt); do
-	npubshort=$(echo -n $npub | grep -o ^npub1................)
-	mkdir -p $npubshort
+  npubshort=$(echo -n $npub | grep -o ^npub1................)
+  mkdir -p $npubshort
   pubkey=$(nak decode $npub | jq -r .pubkey)
-	for relay in $(cat relays.txt); do
-		filename=$npubshort/$npubshort.$relay.jsons
-		if [ ! -e $filename ]; then
-			nak req -k 1 -l 30000 --paginate --paginate-interval 10s \
+  for relay in $(cat relays.txt); do
+    filename=$npubshort/$npubshort.$relay.jsons
+    if [ ! -e $filename ]; then
+      nak req -k 1 -l 30000 --paginate --paginate-interval 10s \
         -a $pubkey $relay | tee $filename
-		fi
-	done
+    fi
+  done
 done
